4b2813071e76b4e7cfe36fe39583306b6feb266c
Kesara Karannagoda <kesarakdkarannagoda@gmail.com>
2021-09-01 20:49:37 +0530

    Add a paragraph to student registration
    Add the paragraph to student registration page.

--- app/views/registration/student.php
+++ app/views/registration/student.php
@@ -5,7 +5,7 @@
  * PROJECT DESCRIPTION: Flexguru is a tutor freelancing platform where students can find the best tutors to fullfill their requirements in a much more flexible manner and provide a platform for enthusiastic tutors to distribute knowledge in whatever the area  they are good at. 
  * @package flexguru
  * AUTHORS: Kavindhu Galagedara
- * CO-AUTHORS: 
+ * CO-AUTHORS: Kesara Karannagoda
  * LAST UPDATE BY: Kavindhu Galagedara
  * LAST UPDATE DATE : Sep, 01, 2021
  * FILE TYPE: View
@@ -19,6 +19,7 @@ include APPROOT . '/views/includes/header.php';
 ?>
 <!-- content  -->
 <h1>Student Registration</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut neque laudantium esse quidem nostrum quos officiis commodi dolor harum nobis ab deleniti, et illum asperiores, voluptate sint velit possimus. Est nisi quibusdam nesciunt eum, suscipit vel debitis delectus officiis voluptatibus deserunt quidem cupiditate labore quasi atque animi consequatur autem omnis.</p>
 <!-- content-end  -->
 <!-- footer  -->
 <?php
