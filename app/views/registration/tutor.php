1bb23aa96f0c65d75cc49b3ad196e3f27622f83c
Avishka <62095876+Avishka217@users.noreply.github.com>
2021-10-03 16:13:56 +0530

    Tutor Registration Improvement
    Created the Tutor Model and the first iteration of the insert operations of Visitor Registration as Tutor

--- app/views/registration/tutor.php
+++ app/views/registration/tutor.php
@@ -12,4 +12,201 @@
  * PARENT FOLDER: registration
  */
 ?>
-<h1>Tutor is working</h1>
+<?php
+
+/**
+ * PROJECT NAME : FLEXGURU
+ * PROJECT DESCRIPTION: Flexguru is a tutor freelancing platform where students can find the best tutors to fullfill their requirements in a much more flexible manner and provide a platform for enthusiastic tutors to distribute knowledge in whatever the area  they are good at.
+ * @package flexguru
+ * AUTHORS: Kesara Karannagoda, Avishka Hettiarachchi, Kavindhu Galagedara, Razeen Nizar
+ * CO-AUTHORS:
+ * LAST UPDATE BY: Kesara Karannagoda
+ * LAST UPDATE DATA : Sep, 01, 2021
+ * FILE TYPE: View
+ * PARENT FOLDER: registration
+ */
+?>
+<?php
+include APPROOT . '/views/includes/data.php';
+include APPROOT . '/views/includes/header.php';
+?>
+
+
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+
+	* {
+		font-family: 'Open Sans', sans-serif;
+	}
+
+
+	.tutor_registration {
+
+		width: 80%;
+		align-items: center;
+		margin: 0 auto;
+		padding: 90px;
+
+	}
+
+	.input-field {
+		margin: 20px 20px 20px 20px;
+	}
+
+	.subscribe {
+		display: grid;
+		width: 50%;
+		grid-template-columns: 20px 500px;
+
+	}
+
+	#checksubscribe {
+		padding: 20px;
+		background: #ff0000;
+		margin-top: 10px;
+	}
+
+
+
+	input[type='text'] {
+		border-radius: 24px;
+		outline: none;
+		border: 1px solid #acacac;
+		width: 350px;
+		padding: 13px 25px;
+	}
+
+	input[type='email'] {
+		border-radius: 24px;
+		outline: none;
+		border: 1px solid #acacac;
+		width: 350px;
+		padding: 13px 25px;
+	}
+
+	input[type='password'] {
+		border-radius: 24px;
+		outline: none;
+		border: 1px solid #acacac;
+		width: 350px;
+		padding: 13px 25px;
+	}
+
+	.button_reg {
+		background: #7879F1;
+		padding: 13px 25px;
+		border: 1px solid #acacac;
+		border-radius: 24px;
+		outline: none;
+		width: 745px;
+		color: white;
+		margin-top: 8px;
+	}
+
+	.join-now {
+		font-size: 13px;
+		margin-top: 7px;
+	}
+</style>
+
+
+
+
+</head>
+
+<body>
+
+<?php   $errors = array('$firstName'=>'','$lastName'=>'','$phoneNo'=>'','$password'=>'','confirmPassword'=>'','NIC'=> '','City'=>'');
+
+?>
+
+<div class="tutor_registration">
+		<center>
+			<h1 style="font-weight:600; font-size:60;">Tutor Registration</h1>
+			<form action="<?php echo URLROOT; ?>/registration/tutor" method="POST">
+				<!-- this two same line -->
+				<table>
+					<thead>
+
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<div class="input-field"> <input type="text" name="firstname" id="firstName" placeholder="FirstName">
+								</div>
+								<span class="form-errors"></span>
+							</td>
+							<td>
+								<div class="input-field"> <input type="text" name="lastname" id="lastName" placeholder="LastName">
+								</div>
+								<span class="form-errors"></span>
+							</td>
+						</tr>
+
+
+						<tr>
+							<td>
+								<div class="input-field"> <input type="email" name="email" id="email" placeholder="Email">
+								</div>
+								<span class="form-errors">  </span>
+							</td>
+							<td>
+								<div class="input-field"> <input type="text" name="phoneno" id="phoneNo" placeholder="Contact Number">
+								</div>
+								<span class="form-errors"></span>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<div class="input-field"> <input type="password" name="password" id="password" placeholder="Password">
+								</div>
+							<td>
+								<div class="input-field"> <input type="password" name="confpassword" id="confirmPassword" placeholder="Confirm Password">
+								</div>
+								<span class="form-errors"></span>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<div class="input-field"> <input type="text" name="gender" id="gender" placeholder="gender">
+								</div>
+								<span class="form-errors"></span>
+							</td>
+							<td>
+								<div class="input-field"> <input type="text" name="city" id="city" placeholder="city">
+								</div>
+								<span class="form-errors"></span>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+
+				<button class="button_reg">CONTINUE</button>
+
+				</br>
+
+				<div class="subscribe">
+
+					<input type="checkbox" id="checksubscribe">
+
+					<div class="join-now"><span>Subscribe to our flexGuru newsletters and agree to receive emails from FlexGuru</span></div>
+
+
+				</div>
+
+			</form>
+		</center>
+	</div>
+</body>
+
+</html>
+
+
+<?php
+include APPROOT . '/views/includes/footer.php';
+?>
+ 
