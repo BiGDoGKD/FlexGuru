0c981d0fbe2c9c8c2dd7f1bcf5a19dd4eeadbc66
razeen ahamed <mnmrrazeen@gmail.com>
2021-10-15 23:36:35 +0530

    page created for Terms and conditions
    Terms and conditions page created in  how -it -works directory to check status

--- app/views/pages/how-it-works.php
+++ app/views/pages/how-it-works.php
@@ -18,11 +18,19 @@ include APPROOT . '/views/includes/data.php';
 include APPROOT . '/views/includes/header.php';
 ?>
 <h1>How it works</h1>
-<?php
-for ($i = 0; $i < 100; $i++) {
-    echo "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit reprehenderit atque ad corrupti possimus hic voluptatem fugit repellat autem! Iste enim magnam hic sed. Itaque saepe aliquam debitis laudantium tenetur iste delectus ducimus minus reprehenderit consequuntur quaerat omnis, vitae expedita harum neque suscipit ex a eius repellat! Quis tenetur quas pariatur aperiam porro vel temporibus debitis commodi, corrupti quam fuga optio, facere sit corporis dignissimos facilis suscipit mollitia maxime molestiae, nesciunt eum soluta dolor! Deserunt facere laudantium nam autem at cum fugit quas? Illo facilis perferendis suscipit sunt adipisci enim. Aliquam sed nemo rerum nesciunt cumque culpa eos, nulla fugit.</p>";
-}
-?>
+<style>
+  p {
+      font-size:24px;
+      font-family:"roboto";
+      letter-spacing:1.5px;
+  }
+</style>
+<body class = "primary w-90 text-justify" >
+
+
+
+</body>
+<br>
 <?php
 include APPROOT . '/views/includes/footer.php';
 ?>
