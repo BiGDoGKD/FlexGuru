e37adbc61dbff50b90355b1b46e57c2a94588d5d
razeen ahamed <mnmrrazeen@gmail.com>
2021-10-15 17:41:14 +0530

    updated about.php
    All needed stylings were done

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -17,28 +17,39 @@
 include APPROOT . '/views/includes/data.php';
 include APPROOT . '/views/includes/header.php';
 ?>
-<h1>About</h1><br><br>
+<link rel="stylesheet" href="<?php echo URLROOT . '/public/css/crystalys-v2.css' ?>">
+<link rel="stylesheet" href="<?php echo URLROOT . '/public/css/visitor/login.css' ?>">
+<style>
+  p {
+      font-size:24px;
+      font-family:"roboto";
+      letter-spacing:1.0px;
+  }
+</style>
+<body class = "bg-on-secondary primary w-90 text-justify" >
+<h1 class = primary>ABOUT</h1> 
 
-<b>Our Vision -</b><br>
- Flexible and Easily accessible education for everyone<br>
+<h3 class =primary>Our Vision</h3>
+ <p>Flexible and Easily accessible education for everyone</p><br>
 
-<b>Who we are</b><br>
-We are a complete resource for current or prospective students, Professionals, and Tutors.
+ <h3 class =primary>Who we are</h3>
+<p>We are a complete resource for current or prospective students, Professionals, and Tutors.
 We give flexibility to the students and professionals to request or choose and learn the exact content they wish to learn. <br>
 Without wasting valuable time by following a complete course or reading an entire book, 
 we offer ranking for the students and tutors to improve their rating to get the best education while enjoying benefits from Flexguru. <br>
 Customized learning requests are one of the uniqueness of our platform.
-Every student will be able to request the tutor while mentioning his needs and preferences in order to succeed easily.<br>
+Every student will be able to request the tutor while mentioning his needs and preferences in order to succeed easily.<br></p>
 
-<b>How it all started</b><br>
+<h3 class =primary>How it all started</h3>
 
-We found that there is an issue in the common E-education system after the pandemic strikes and students moving towards online learning. <br>
+<p>We found that there is an issue in the common E-education system after the pandemic strikes and students moving towards online learning. <br>
 "Without a problem, there is no innovation" in that perspective the problem we figured out is, students and professionals get doubts in specific parts of the subject 
 in the learning process but there are no perfect solutions to overcome it. According to our country, every student does not get enough resources while learning and clearing doubts. <br>
-so we group of undergraduate students came up with this idea to help students and professionals to give an easily accessible and flexible education as our vision suggests with a personalized learning dashboard.<br>
+so we group of undergraduate students came up with this idea to help students and professionals to give an easily accessible and flexible education as our vision suggests with a personalized learning dashboard.<br></p>
 
 
-
+</body>
+<br>
 
 
 
