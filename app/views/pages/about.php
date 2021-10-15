02475cd48628e98ce9bc3ad83d8e38d745674d16
razeen ahamed <mnmrrazeen@gmail.com>
2021-10-15 19:42:22 +0530

    Completed About & Privacy pages
    Need to rename how to become a student to privacy policy

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -23,30 +23,32 @@ include APPROOT . '/views/includes/header.php';
   p {
       font-size:24px;
       font-family:"roboto";
-      letter-spacing:1.0px;
+      letter-spacing:1.5px;
   }
 </style>
-<body class = "bg-on-secondary primary w-90 text-justify" >
-<h1 class = primary>ABOUT</h1> 
+<body class = "primary w-90 text-justify" >
+<h1 class = primary>ABOUT US</h1> 
 
 <h3 class =primary>Our Vision</h3>
- <p>Flexible and Easily accessible education for everyone</p><br>
+<br>
+ <p>Flexible and Easily accessible education for everyone.</p><br>
 
  <h3 class =primary>Who we are</h3>
+ <br>
 <p>We are a complete resource for current or prospective students, Professionals, and Tutors.
 We give flexibility to the students and professionals to request or choose and learn the exact content they wish to learn. <br>
 Without wasting valuable time by following a complete course or reading an entire book, 
 we offer ranking for the students and tutors to improve their rating to get the best education while enjoying benefits from Flexguru. <br>
 Customized learning requests are one of the uniqueness of our platform.
 Every student will be able to request the tutor while mentioning his needs and preferences in order to succeed easily.<br></p>
-
+<br>
 <h3 class =primary>How it all started</h3>
-
+<br>
 <p>We found that there is an issue in the common E-education system after the pandemic strikes and students moving towards online learning. <br>
 "Without a problem, there is no innovation" in that perspective the problem we figured out is, students and professionals get doubts in specific parts of the subject 
 in the learning process but there are no perfect solutions to overcome it. According to our country, every student does not get enough resources while learning and clearing doubts. <br>
 so we group of undergraduate students came up with this idea to help students and professionals to give an easily accessible and flexible education as our vision suggests with a personalized learning dashboard.<br></p>
-
+<br>
 
 </body>
 <br>
