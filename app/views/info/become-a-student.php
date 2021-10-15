f5f56fa38cd924cc6e5c112554034782ee202433
razeen ahamed <mnmrrazeen@gmail.com>
2021-10-15 18:55:51 +0530

    updating privacy policy
    Content is added in become a student page to avoid conflicts

--- app/views/info/become-a-student.php
+++ app/views/info/become-a-student.php
@@ -16,12 +16,61 @@
 include APPROOT . '/views/includes/data.php';
 include APPROOT . '/views/includes/header.php';
 ?>
-<h1>Become a Student</h1>
-<?php
-for ($i = 0; $i < 100; $i++) {
-    echo "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit reprehenderit atque ad corrupti possimus hic voluptatem fugit repellat autem! Iste enim magnam hic sed. Itaque saepe aliquam debitis laudantium tenetur iste delectus ducimus minus reprehenderit consequuntur quaerat omnis, vitae expedita harum neque suscipit ex a eius repellat! Quis tenetur quas pariatur aperiam porro vel temporibus debitis commodi, corrupti quam fuga optio, facere sit corporis dignissimos facilis suscipit mollitia maxime molestiae, nesciunt eum soluta dolor! Deserunt facere laudantium nam autem at cum fugit quas? Illo facilis perferendis suscipit sunt adipisci enim. Aliquam sed nemo rerum nesciunt cumque culpa eos, nulla fugit.</p>";
-}
-?>
+<link rel="stylesheet" href="<?php echo URLROOT . '/public/css/crystalys-v2.css' ?>">
+<link rel="stylesheet" href="<?php echo URLROOT . '/public/css/visitor/login.css' ?>">
+<style>
+  p {
+      font-size:24px;
+      font-family:"roboto";
+      letter-spacing:1.0px;
+  }
+</style>
+<body class = "bg-on-secondary primary w-90 text-justify" >
+<h1 class = primary>ABOUT</h1> 
+
+<h3 class =primary>privacy policy of Flexguru
+</h3>
+ <p>We’re deeply committed to creating a safe and secure online environment for you.</p><br>
+
+ <h3 class =primary>What are the data we get</h3>
+<p>We collect certain data from you directly, like the information you enter yourself, 
+    data about your consumption of content, and data from third-party platforms you connect with Flexguru. 
+    We also collect some data automatically, like information about your device and what parts of our 
+    Services you interact with or spend time using.
+</p>
+
+<h3 class =primary>Account Data</h3>
+
+<p>In order to use certain features (like accessing content), you need to create a user account through a registration process. When you create or update your account, we collect and store the data you provide, like your email address, password, phone number, schools, skill interests, gender, government ID information(In some cases), verification photo, age, date of birth, and account settings and assign you a unique identifying number.</p>
+
+<h3 class =primary>Profile Data</h3>
+
+<p>You can also choose to provide profile information like a photo, Description, language, website link, social media profiles, Town, or other data. Your Profile Data will be publicly viewable by others. (In some cases we made it optional)
+
+We will keep the Personal Information of our users for as long as they are registered subscribers or users of our products and services, and as permitted by law.
+</p>
+
+<h3 class =primary>Payment data</h3>
+
+<p>All the payments & Transactions were done through a recognized local payment sandbox named pay here and all the transactions and related data were only shared with them.</p>
+<p>If you choose to register for our products and services, we will send you certain promotional emails. Promotional emails advertise our products and services and/or the products and services of our Users and Affiliates. If you do not want to receive promotional emails from us, you may elect to opt out of receiving promotional emails at any time by hitting the "unsubscribe" button at the bottom of any of our emails.</p>
+<h3 class =primary>Cookies</h3>
+
+<p>We use cookies, which are small text files stored by your browser, to collect, store, and share data about your activities across websites, including on Flexguru. They allow us to remember things about your visits to Udemy, like your preferred language, and to make the site easier to use. by giving an advantage to affiliate marketers.
+To learn more about cookies, visit https://cookiepedia.co.uk/all-about-cookies.
+</p>
+<p>We do not sell your personal information to third parties. We established Flexguru as a service-oriented organization so that our mission of education and your trust will not be in conflict with a for-profit motive.</p>
+
+<p>We do not display advertising on Flexguru. Our mission is to provide you with a world-class education, not to sell you products</p>
+<h3 class =primary>How it all started</h3>
+
+<p></p>
+
+</body>
+<br>
+
+
+
 <?php
 include APPROOT . '/views/includes/footer.php';
 ?>
