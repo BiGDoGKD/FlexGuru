08dbcc50ee7a826a1fd1333424962411a6c3fb6e
razeen ahamed <mnmrrazeen@gmail.com>
2021-10-15 23:59:21 +0530

    completed 3 information pages
    Change the directory names

--- app/views/pages/how-it-works.php
+++ app/views/pages/how-it-works.php
@@ -17,16 +17,18 @@
 include APPROOT . '/views/includes/data.php';
 include APPROOT . '/views/includes/header.php';
 ?>
-<h1>How it works</h1>
+
 <style>
   p {
       font-size:24px;
       font-family:"roboto";
       letter-spacing:1.5px;
+      margin-top: 1em;
+      margin-bottom: 1em;
   }
 </style>
 <body class = "primary w-90 text-justify" >
-
+<h2>Terms and Conditions of our platform</h1>
 <p>1. These terms and conditions guide how we provide you with our services, including eLearning sessions.    </p>
 <p>2. Before submitting your registration form to us, please read these terms carefully.     </p>
 <p>3. You are advised to check our Websites regularly to view our most recent privacy policy
@@ -35,17 +37,52 @@ agreement to ensure that you are aware of any changes to it because that may occ
 <p>4. We develop, distribute and maintain the Product and will also provide login information to you. 
     We will also manage your access to the services and provide you with assistance if needed. 
     </p>
-<p>5.You do not copy, modify, transmit, distribute or in any way exploit the Product or any other copyrighted 
+<p>5. You do not copy, modify, transmit, distribute or in any way exploit the Product or any other copyrighted 
     material provided other than for your training purposes. any other purposes are strictly prohibited by these conditions.     </p>
-<p>6.You strictly do not permit anyone to copy, use, modify, transmit, distribute, or otherwise exploit the Products or any other copyrighted material in any way.     </p>
-<p>7.We provide the material as-is and without any warranties, express or implied, except that which cannot be excluded by law.
+<p>6. You strictly do not permit anyone to copy, use, modify, transmit, distribute, or otherwise exploit the Products or any other copyrighted material in any way.     </p>
+<p>7. We provide the material as-is and without any warranties, express or implied, except that which cannot be excluded by law.
      We also do not guarantee that the materials will be free from errors, including technical errors.    </p>
-<p>8.We use third-party payment providers, depending on the way in which you make payments – Pay here. Payment for the Products must be made at the point of purchase.     </p>
-<p>    </p>
-<p>    </p>
-<p>    </p>
-<p>    </p>
-<p>    </p>
+<p>8. We use third-party payment providers, depending on the way in which you make payments – Pay here. Payment for the Products must be made at the point of purchase.     </p>
+<p>9. Please note that we must receive your payment in full before providing you with access to all types of services our platform provides.     </p>
+<p>10.Your payment includes the licensing of the Products for a limited period of time that is appropriate for the eLearning content.
+    </p>
+<p>11.We are unable to provide a refund if you fail to complete the content within the allocated time, except at our absolute and sole discretion.     </p>
+<p>12.We reserve our right to review and change the pricing of any of our products. This will not affect products that have already been purchased.
+    </p>
+<p>13.You must be a human to open an account. Accounts registered by “bots” or other automated methods are not permitted
+    </p>
+    <p>14.You are responsible for maintaining the security of your account and password. 
+        We view any actions taken by your account as taken by you. Flexguru will not be liable for any loss or damage from your failure to comply
+         with this security obligation
+</p>
+<p>15.You will not partake in any behavior that victimizes, harasses, degrades, or intimidates an individual or group of 
+    individuals on the basis of religion, gender, sexual orientation, race, ethnicity, age, or disability
+</p>
+<p>16.You will not share software viruses or any other computer code, files, or programs that are designed or 
+    intended to disrupt, damage, or limit the functioning of any software, hardware, or telecommunications equipment 
+    or to damage or obtain unauthorized access to any data or other information of any third party.
+</p>
+<p>17.Teachers may earn revenue by publishing classes or offering special services through Flexguru. Payment will be deposited for every session.
+
+</p>
+<p>
+18.All security on our Website is treated seriously. Where applicable, we undertake security steps, including the use 
+of SSL technology, on our back-end systems that store customer account information and protect data transmissions. However, this is not a 
+guarantee that such data transmissions cannot be accessed, altered, or deleted due to firewall or other security software failures. 
+If you have any further concerns about security, please email our Customer Service team at support@Flexguru.lk
+</p>
+<p>
+19.We use personal information to allow us to process your service requests, provide access to privileged areas of the Websites, 
+send out newsletters, personalize your visit to our Websites and enable us to improve the services we offer. We may occasionally carry out 
+market research and send you details of services and offers that we think may be of interest to you. If you do not wish to receive such information, 
+please email support@flexguru.lk or, alternatively, when we send you an email, 
+it will contain a provision for you to opt out of receiving any further information from us. 
+</p>
+<p>
+20.All the ranking processes of Tutors and Students are done through a dedicated Flexguru algorithm and no individual involvements
+ or requests are strictly prohibited to make changes in  the ranking process
+
+</p>
 </body>
 <br>
 <?php
