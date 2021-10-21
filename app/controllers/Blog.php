bf2f072cef9cbac8b818744807ff147a0afe70de
Avishka <62095876+Avishka217@users.noreply.github.com>
2021-10-21 20:55:50 +0530

    blog create
    blog create

--- app/controllers/Blog.php
+++ app/controllers/Blog.php
@@ -4,20 +4,22 @@ class Blog extends Controller {
         $this->postModel = $this->model('Post');
     }
 
+
+
+
     public function index() {
+
         $posts = $this->postModel->findAllPosts();
 
         $data = [
             'posts' => $posts
         ];
 
-        $this->view('posts/index', $data);
+        $this->view('blog/index', $data);
     }
 
     public function create() {
-       /* if(!isLoggedIn()) {
-            header("Location: " . URLROOT . "/posts");
-        } */
+
 
         $data = [
             'title' => '',
@@ -30,7 +32,7 @@ class Blog extends Controller {
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
             $data = [
-                'user_id' => $_SESSION['user_id'],
+                'user_id' => 104,
                 'title' => trim($_POST['title']),
                 'body' => trim($_POST['body']),
                 'titleError' => '',
@@ -47,27 +49,22 @@ class Blog extends Controller {
 
             if (empty($data['titleError']) && empty($data['bodyError'])) {
                 if ($this->postModel->addPost($data)) {
-                    header("Location: " . URLROOT . "/posts");
+                    header('location:' . URLROOT . '/blog/index');
                 } else {
                     die("Something went wrong, please try again!");
                 }
-            } else {
-                $this->view('posts/create', $data);
-            }
+            } 
         }
 
-        $this->view('posts/create', $data);
+        $this->view('blog/create', $data);
     }
 
-    public function update($id) {
+
+    public function update($id)
+    {
 
         $post = $this->postModel->findPostById($id);
 
-        if(!isLoggedIn()) {
-            header("Location: " . URLROOT . "/posts");
-        } elseif($post->user_id != $_SESSION['user_id']){
-            header("Location: " . URLROOT . "/posts");
-        }
 
         $data = [
             'post' => $post,
@@ -77,76 +74,168 @@ class Blog extends Controller {
             'bodyError' => ''
         ];
 
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
             $data = [
                 'id' => $id,
                 'post' => $post,
-                'user_id' => $_SESSION['user_id'],
+                'user_id' => 104,
                 'title' => trim($_POST['title']),
                 'body' => trim($_POST['body']),
                 'titleError' => '',
                 'bodyError' => ''
             ];
 
-            if(empty($data['title'])) {
+            if (empty($data['title'])) {
                 $data['titleError'] = 'The title of a post cannot be empty';
             }
 
-            if(empty($data['body'])) {
+            if (empty($data['body'])) {
                 $data['bodyError'] = 'The body of a post cannot be empty';
             }
 
-            if($data['title'] == $this->postModel->findPostById($id)->title) {
-                $data['titleError'] == 'At least change the title!';
+
+
+
+            if ($data['title'] == $this->postModel->findPostById($id)->title) {
+                $data['titleError'] = 'At least change the title!';
             }
 
-            if($data['body'] == $this->postModel->findPostById($id)->body) {
-                $data['bodyError'] == 'At least change the body!';
+            if ($data['body'] == $this->postModel->findPostById($id)->body) {
+                $data['bodyError'] = 'At least change the body!';
             }
 
             if (empty($data['titleError']) && empty($data['bodyError'])) {
                 if ($this->postModel->updatePost($data)) {
-                    header("Location: " . URLROOT . "/posts");
+                    header("Location: " . URLROOT . "/blog/index");
                 } else {
                     die("Something went wrong, please try again!");
                 }
-            } else {
-                $this->view('posts/update', $data);
-            }
+            } 
         }
 
-        $this->view('posts/update', $data);
+        $this->view('blog/update', $data);
     }
 
-    public function delete($id) {
 
-        $post = $this->postModel->findPostById($id);
 
-        if(!isLoggedIn()) {
-            header("Location: " . URLROOT . "/posts");
-        } elseif($post->user_id != $_SESSION['user_id']){
-            header("Location: " . URLROOT . "/posts");
-        }
 
-        $data = [
-            'post' => $post,
-            'title' => '',
-            'body' => '',
-            'titleError' => '',
-            'bodyError' => ''
-        ];
 
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-            if($this->postModel->deletePost($id)) {
-                    header("Location: " . URLROOT . "/posts");
-            } else {
-               die('Something went wrong!');
-            }
-        }
-    }
+//     public function update($id)
+//     {
+
+//         $post = $this->postModel->findPostById($id);
+
+//         if (!isLoggedIn()) {
+//             header("Location: " . URLROOT . "/blog");
+//         } elseif ($post->user_id != $_SESSION['user_id']) {
+//             header("Location: " . URLROOT . "/blog");
+//         }
+
+//         $data = [
+//             'post' => $post,
+//             'title' => '',
+//             'body' => '',
+//             'titleError' => '',
+//             'bodyError' => ''
+//         ];
+
+//         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+//             $data = [
+//                 'id' => $id,
+//                 'post' => $post,
+//                 'user_id' => $_SESSION['user_id'],
+//                 'title' => trim($_POST['title']),
+//                 'body' => trim($_POST['body']),
+//                 'titleError' => '',
+//                 'bodyError' => ''
+//             ];
+
+//             if (empty($data['title'])) {
+//                 $data['titleError'] = 'The title of a post cannot be empty';
+//             }
+
+//             if (empty($data['body'])) {
+//                 $data['bodyError'] = 'The body of a post cannot be empty';
+//             }
+
+//             if ($data['title'] == $this->postModel->findPostById($id)->title) {
+//                 $data['titleError'] == 'At least change the title!';
+//             }
+
+//             if ($data['body'] == $this->postModel->findPostById($id)->body) {
+//                 $data['bodyError'] == 'At least change the body!';
+//             }
+
+//             if (empty($data['titleError']) && empty($data['bodyError'])) {
+//                 if ($this->postModel->updatePost($data)) {
+//                     header("Location: " . URLROOT . "/blog");
+//                 } else {
+//                     die("Something went wrong, please try again!");
+//                 }
+//             } else {
+//                 $this->view('blog/update', $data);
+//             }
+//         }
+
+//         $this->view('blog/update', $data);
+//     }
+
+//     public function delete($id)
+//     {
+
+//         $post = $this->postModel->findPostById($id);
+
+//         if (!isLoggedIn()) {
+//             header("Location: " . URLROOT . "/blog");
+//         } elseif ($post->user_id != $_SESSION['user_id']) {
+//             header("Location: " . URLROOT . "/blog");
+//         }
+
+//         $data = [
+//             'post' => $post,
+//             'title' => '',
+//             'body' => '',
+//             'titleError' => '',
+//             'bodyError' => ''
+//         ];
+
+//         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+//             if ($this->postModel->deletePost($id)) {
+//                 header("Location: " . URLROOT . "/blog");
+//             } else {
+//                 die('Something went wrong!');
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // class end tag
 }
 
+
+
+
+
+
+
+   
--- app/views/blog/update.php
+++ app/views/blog/update.php
@@ -1,25 +1,14 @@
 <?php
-   require APPROOT . '/views/includes/header.php';
+include APPROOT . '/views/admin/components/admin-header.php';
 ?>
 
-<div class="navbar">
-    <?php
-       require APPROOT . '/views/includes/navigation-visitor.php';
-    ?>
-</div>
 
-<div class="container center">
-    <h1>
-        Update post
-    </h1>
 
-    <form
-    action="<?php echo URLROOT; ?>/posts/update/<?php echo $data['post']->id ?>" method="POST">
+
+
+    <form action="<?php echo URLROOT; ?>/blog/update" method="POST">
         <div class="form-item">
-            <input
-                type="text"
-                name="title"
-                value="<?php echo $data['post']->title ?>">
+            <input type="text" name="title" value="<?php echo $data['post']->title ?>">
 
             <span class="invalidFeedback">
                 <?php echo $data['titleError']; ?>
