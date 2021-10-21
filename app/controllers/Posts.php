c42b0c260c4952b127aafb32926c0c351f75d033
Kavindhu G <62440935+Kav-G@users.noreply.github.com>
2021-10-21 12:16:18 +0530

    Update Posts.php app/controllers
    Updated Posts.php in the "controllers" directory

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -3,3 +3,13 @@ class Posts extends Controller {
     public function __construct() {
         $this->postModel = $this->model('Post');
     }
+
+    public function index() {
+        $posts = $this->postModel->findAllPosts();
+
+        $data = [
+            'posts' => $posts
+        ];
+
+        $this->view('posts/index', $data);
+    }
