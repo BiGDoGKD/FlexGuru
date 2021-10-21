33200f1e7cf8c07f28f10147d35966d9984277ac
Kavindhu G <62440935+Kav-G@users.noreply.github.com>
2021-10-21 12:03:45 +0530

    Update Post.php app/views
    Read all the posts from the database and return them as an array in an ascending order.

--- app/models/Post.php
+++ app/models/Post.php
@@ -5,3 +5,11 @@ class Post {
     public function __construct() {
         $this->db = new Database;
     }
+
+    public function findAllPosts() {
+        $this->db->query('SELECT * FROM posts ORDER BY created_at ASC');
+
+        $results = $this->db->resultSet();
+
+        return $results;
+    }
