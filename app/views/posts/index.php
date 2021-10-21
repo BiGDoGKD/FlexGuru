fb0d01433efc8334d3a59b1741f1c32c95c46e56
Kavindhu G <62440935+Kav-G@users.noreply.github.com>
2021-10-21 12:43:30 +0530

    Create index.php app/views/posts
    Created index.php on app/views/posts and applied style.

---
+++ app/views/posts/index.php
@@ -0,0 +1,29 @@
+<?php
+   require APPROOT . '/views/includes/header.php';
+?>
+
+<style>
+
+/* Blog */
+.dark {
+    background-color: #000000;
+    box-shadow: 0px 0px 10px #000000;
+}
+
+.container {
+    margin: 0 auto;
+    width: 80%;
+    padding: 100px 0px;
+}
+
+.container-item {
+    border-bottom: 1px solid #dcdcdc;
+}
+
+</style>
+
+<div class="navbar dark">
+    <?php
+       require APPROOT . '/views/includes/navigation-visitor.php';
+    ?>
+</div>
