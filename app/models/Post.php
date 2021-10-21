e83ea8ac2e58e8844f3ad05574ec52cd0afd8dd9
Kavindhu G <62440935+Kav-G@users.noreply.github.com>
2021-10-21 17:02:08 +0530

    Update Posts.php and Post.php
    Update Posts.php in app/controllers and Post.php  in app/models.

--- app/models/Post.php
+++ app/models/Post.php
@@ -14,4 +14,53 @@ class Post {
         return $results;
     }
 
+    public function addPost($data) {
+        $this->db->query('INSERT INTO posts (user_id, title, body) VALUES (:user_id, :title, :body)');
+
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':body', $data['body']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function findPostById($id) {
+        $this->db->query('SELECT * FROM posts WHERE id = :id');
+
+        $this->db->bind(':id', $id);
+
+        $row = $this->db->single();
+
+        return $row;
+    }
+
+    public function updatePost($data) {
+        $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+
+        $this->db->bind(':id', $data['id']);
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':body', $data['body']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function deletePost($id) {
+        $this->db->query('DELETE FROM posts WHERE id = :id');
+
+        $this->db->bind(':id', $id);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }    
