80db01fff8dc079738084dd835b362c20a0debe7
Kesara Karannagoda <kesarakdkarannagoda@gmail.com>
2021-10-22 07:03:17 +0530

    Update admin dashboard

--- app/views/admin/pages/payments.php
+++ app/views/admin/pages/payments.php
@@ -1,1 +1,3 @@
-<h1>Payments</h1>
+<div style="display:flex;justify-content:center;align-items:center; width:100%;height:100%;">
+    <img height="80%" width="80%" src="<?php echo URLROOT . '/public/img/admin/payments.png' ?>" alt="">
+</div>
