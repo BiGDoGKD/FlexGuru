025e7752514ccd628d7209936a13287a04e4e2df
Avishka <62095876+Avishka217@users.noreply.github.com>
2021-11-05 20:56:01 +0530

    FAQ Page updated
    FAQ Page updated

--- app/config/config.php
+++ app/config/config.php
@@ -14,7 +14,7 @@
 //Database params
 define('DB_HOST', 'localhost'); //Add your db host
 define('DB_USER', 'root'); // Add your DB root
-define('DB_PASS', 'Kesara1!'); //Add your DB pass
+define('DB_PASS', 'root'); //Add your DB pass
 define('DB_NAME', 'api'); //Add your DB Name
 
 //APPROOT
