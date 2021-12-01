962015ad415f2213ff3d3459d4594a0b4fef99d5
Razeen <65141554+mnmrazeen@users.noreply.github.com>
2021-12-01 20:40:40 +0530

    created become a student page
    removed become a student page from pages and added new file in info directory.function created but unable to see become a student in json file so not connected.check it

---
+++ app/views/info/becomeastudent.php
@@ -0,0 +1,132 @@
+<?php
+include APPROOT . '/views/includes/data.php';
+include APPROOT . '/views/includes/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How to become a Tutor</title>
+    <link rel="stylesheet" href="/public/css/crystalys-v2.css">
+    
+    <style>
+        div {
+            width: 100%;
+            display: flex;
+            margin-top: 50px;
+            margin-bottom: 50px;
+        }
+        #pic1{
+                height: auto;
+                width: 75%;
+                margin-left: 10%;
+        }
+        #pic2{
+                height: auto;
+                width: 90%;
+        }
+        #pic3{
+
+                height: auto;
+                width: 90%;
+            
+        }
+        #pic4{
+                height: auto;
+                width: 90%;
+            
+        }
+        .div-l{
+            width: 50%;
+            height: auto;
+
+        }
+        .div-r{
+            width: 50%;
+            height: auto;
+        }
+        
+       
+    </style>
+</head>
+<body class="body-1 primary bg-background text-justify">
+<h1>Welcome to FlexGuru</h1>
+<div>
+    <div class="div-l">
+        <p class="subtitle-1"> We designed FlexGuru to open doors of oppurtunity to all Students ,Tutors and Professionals in our country so you can provide your service with ease while earning money.<br><br>
+            Flexguru provides variety of areas to learn so students have the flexibility to learn most of the important things through our platform.<br><br>
+            We are using a unique ranking system to give you extra rewards and benifits if you use ur platform effectively.<br><br>
+            Special service request gives you a next level learning experince while giving you a complete flexibility.<br><br>
+            You can make your payments from home with our secure payment options<br><br></p>
+    </div>
+    <div class="div-r">
+        <img src="images/stu2.svg" id="pic1">
+    </div>
+</div>
+<div>
+    <div class="div-l">
+        <h1>Why you have to choose FlexGuru... </h1>
+    </div>
+    <div class="div-r">
+        <ul>
+            <li>
+                <h3>Wide variety of areas to learn</h3>
+                <p>Our platform is not limited to specific areas of subjects like other E-learning platforms.</p>
+            </li>
+            <li>
+                <h3>Ranking process gives extra advantages</h3>
+                <p>Ranking process gives extra advantage to loyal Students of our platform</p>
+            </li>
+            <li>
+                <h3>Flexible and customisable shedules </h3>
+                <p>Tutors and students can chat and able to fix a flexible time to the sessions</p>
+            </li>
+            <li>
+                <h3>Secure payment options through a secured gateway.</h3>
+                <p>Students can make their payments any time using our payment gateway with many transwer options.</p>
+            </li>
+    </ul>
+    </div>
+</div>
+<div>
+    <div class="div-l">
+        <img src="images/tut1.jpg" id="pic2">
+    </div>
+    <div class="div-r">
+        <p>
+            1.Just visit Flexguru website and Choose to register as a Student.<br><br>
+            2.Then you need to give some regular details in the form to sign up as a student<br><br>
+            3.You have the flexibility change your role depend on your situation any time.<br><br>
+            4.No registration fees requred to join and explore our FlexGuru platform.<br><br>
+            
+        </p>
+    </div>
+</div>
+
+<div>
+    <div class="div-l"><img src="images/tut2.jpg" id="pic3"></div>
+    <div class="div-r"><p>
+        5.Students do not need to provide any documents after filling the registration form<br><br>
+        6.After registering as a Student you have to verify your mail using the unique OTP sent to your mail.<br><br>
+        7.You will recieve a five digit OTP to verify your Email.<br><br>
+        8.If you are registered already then you can login to our system with your user name and password directly from here.<br><br>
+    </p></div>
+</div>
+<div>
+    <div class="div-l"><img src="images/mail.png"id="pic4"></div>
+    <div class="div-r">
+    <p>
+        OTP is unique to you so dont share it with anyone<br><br>
+        if you didnt recieved OTP withing 5 min recheck your E mail ID<br><br>
+        Contact us if you have any issues we are here to help you anytime<br><br>
+        Happy learning<br><br>
+    </p>
+</div>
+</div>
+</body>
+<?php
+include APPROOT . '/views/includes/footer.php';
+?>
+</html>
