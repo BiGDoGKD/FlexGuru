525e061b502066ad6b656f1209b51931932613d3
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-02-18 08:20:21 +0530

    fix: update email verification

--- app/views/registration/email.php
+++ app/views/registration/email.php
@@ -1,77 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-/** 
- * PROJECT NAME : FLEXGURU
- * PROJECT DESCRIPTION: Flexguru is a tutor freelancing platform where students can find the best tutors to fullfill their requirements in a much more flexible manner and provide a platform for enthusiastic tutors to distribute knowledge in whatever the area  they are good at. 
- * @package flexguru
- * AUTHORS: Kesara Karannagoda, Avishka Hettiarachchi, Kavindhu Galagedara, Razeen Nizar
- * CO-AUTHORS: 
- * LAST UPDATE BY: Kesara Karannagoda
- * LAST UPDATE DATA : Sep, 01, 2021
- * FILE TYPE: View
- * PARENT FOLDER: registration
- */
-?>
-
-<?php
-include APPROOT . '/views/includes/data.php';
-include APPROOT . '/views/includes/header.php';
-?>
-<!-- content  -->
-<style>
-    #email-verification {
-        display: flex;
-        flex-direction: column;
-        padding: 50px 0;
-        justify-content: center;
-    }
-
-    #email-verification form input {
-        width: 300px;
-        height: 70px;
-        margin-top: 15px;
-        margin-bottom: 5px;
-        padding: 0 0 0 20px;
-        border: 1px solid #ACACAC;
-        box-sizing: border-box;
-        border-radius: 24px;
-        font-size: 45px;
-        font-weight: bold;
-        letter-spacing: 5px;
-        color: var(--gray);
-    }
-
-    #email-verification .button_verify {
-        padding: 10px 100px;
-        margin: 5px 0;
-    }
-
-    #email-verification .resend-button {
-        margin: 10px 0 50px 0;
-    }
-</style>
-
-
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Flexguru Roles</title>
+    <?php include APPROOT . '/views/includes/header.php'; ?>
 </head>
 
 <body>
-    <div id="email-verification">
-        <h3>Email Verification</h3>
-        <p class="subtitle-1">An email verification code was sent to <?php echo $data['email'] ?></p>
-        <form action="<?php echo URLROOT; ?>/registration/verification" class="verification" method="POST">
-            <input type="text" name="emailcode" id="emailcode">
-            <p class="subtitle-1 error"><?php echo $data['otpError'] ?></p>
-            <button class="btn button_verify pointer on-primary bg-primary btn-text" id="submit" value="submit" type="submit">CONTINUE</button>
-        </form>
-        <h5 style="margin:35px 0 10px 0;">Having trouble receiving an account verification code?</h5>
-        <p class="subtitle-1 gray">It coult take up some time code to be delivered</p>
-        <button class="btn bg-primary on-primary resend-button" style="width: 400px;">RESEND ACCOUNT VERIFICATION CODE</button>
+    <?php
+    include APPROOT . '/views/includes/navbar.php';
+    ?>
+    <div class="container student-registration pt-lg-4 pt-xs-2 pb-4" id="email-verification">
+        <div class="row">
+            <div class="col-12-xs">
+                <p class="text-center font-lg-xxl font-xs-lg mb-xs-1 mb-lg-2">
+                    Email Verification
+                </p>
+            </div>
+
+            <form action="<?php echo URLROOT; ?>/registration/verification" method="POST" class="form-control row" style="width: 100%">
+                <div class="form-group col-12-xs">
+                    <p class="text-gray mb-1">An email verification code was sent to <span class="text-primary"><?php echo $data['email'] ?></span></p>
+                    <input type="text" class="form-control" name="emailcode" id="emailcode" placeholder="Enter the OTP" required />
+                    <p class="form-control form-feedback text-error">
+                        <?php echo $data['otpError'] ?>
+                    </p>
+                </div>
+                <div class="form-group col-12-xs">
+                    <button type="submit" id="submit" name="submit" value="submit" class="form-control">
+                        Continue
+                    </button>
+                </div>
+            </form>
+            <div class="col-12-xs form-control mt-3">
+                <p class="font-sm text-gray">Having trouble receiving an account verification code?</p>
+                <p class="text-gray mt-1">
+                    It could take some time code to be delivered
+                </p>
+                <button class="form-control mt-1" style="padding: 0.5rem 2rem;"><span class="font-sm">RESEND ACCOUNT VERIFICATION CODE</span></button>
+            </div>
+        </div>
     </div>
+    <?php
+    include APPROOT . '/views/includes/footer.php';
+    ?>
 </body>
 
 </html>
-<!-- content-end  -->
-<!-- footer  -->
-<?php
-include APPROOT . '/views/includes/footer.php';
-?>
