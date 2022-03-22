2a3c9a3558969d0389e3b60b505b73b40bcbdff5
Avishka <62095876+Avishka217@users.noreply.github.com>
2022-03-22 08:47:27 +0530

    Settings pages of Student , Tutor and Affiliate Marketer updated
    Settings pages of Student , Tutor and Affiliate Marketer updated

--- app/views/tutor/settings.php
+++ app/views/tutor/settings.php
@@ -19,100 +19,100 @@
     ?>
 
 
-        <div class="user-dashboard container base-container mt-3 mb-3">
+    <div class="user-dashboard container base-container mt-3 mb-3">
 
-            <div class="gig-form col-12-xs col-8-lg">
-                <form action="<?php echo URLROOT; ?>/gigs/publish" method="POST" style="width: 100%;" class="form-control row">
-                    <div class="form-group col-12-xs">
-                        <label for="firstname">First Name<span class="text-error">*</span> </label>
-                        <input type="text" class="form-control" name="firstname" id="firstname" placeholder="Enter the FirstName..." value="" required />
-                        <p class="form-control form-feedback text-error">
+        <div class="gig-form col-12-xs col-8-lg">
+            <form action="<?php echo URLROOT; ?>/gigs/publish" method="POST" style="width: 100%;" class="form-control row">
+                <div class="form-group col-12-xs">
+                    <label for="firstname">First Name<span class="text-error">*</span> </label>
+                    <input type="text" class="form-control" name="firstname" id="firstname" placeholder="Enter the FirstName..." value="" required />
+                    <p class="form-control form-feedback text-error">
 
-                        </p>
-                    </div>
-
-
-
-                    <div class="form-group col-12-xs">
-                        <label for="Lastname">Last Name<span class="text-error">*</span> </label>
-                        <input type="text" class="form-control" name="title" id="title" placeholder="Enter the Last Name..." value="" required />
-                        <p class="form-control form-feedback text-error">
-
-                        </p>
-                    </div>
-
-                    <div class="form-group col-12-xs">
-                        <label for="email">Email<span class="text-error">*</span> </label>
-                        <input type="text" class="form-control" name="email" id="email" placeholder="ex :abc@yahoo.com" value="" required />
-                        <p class="form-control form-feedback text-error">
-
-                        </p>
-                    </div>
-
-
-
-                    <div class="form-group col-12-xs">
-                        <label for="phoneno">Phoneno<span class="text-error">*</span> </label>
-                        <input type="text" class="form-control" name="phoneno" id="phoneno" placeholder="Enter the phone number" value="" required />
-                        <p class="form-control form-feedback text-error">
-
-                        </p>
-                    </div>
-
-
-
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <div class="accountdeletion">
-                <div class="subtitle" id="act_del">
-                    <p class="text-gray">Account Deletion </p>
-                </div>
-                <div class="deletiondetails" style="width: 100%;">
-
-
-
-
-
-                </div>
-
-
-
-
-
-
-            </div>
-
-            <div class="deletereason">
-                <div class="subtitle">
-                    <p class="text-gray">I am deleting my account because (optional)
                     </p>
                 </div>
-                <textarea id="deletereason" name="deletereason" rows="4" cols="50" style="resize:none;">
-</textarea>
-
-
-            </div>
 
 
 
-            <div class="col-4-xs display-f ">
-                <button onclick="location.href='http://localhost/info/help'" class="marketplace__help-btn">Delete My Account </button>
-            </div>
+                <div class="form-group col-12-xs">
+                    <label for="Lastname">Last Name<span class="text-error">*</span> </label>
+                    <input type="text" class="form-control" name="title" id="title" placeholder="Enter the Last Name..." value="" required />
+                    <p class="form-control form-feedback text-error">
+
+                    </p>
+                </div>
+
+                <div class="form-group col-12-xs">
+                    <label for="email">Email<span class="text-error">*</span> </label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="ex :abc@yahoo.com" value="" required />
+                    <p class="form-control form-feedback text-error">
+
+                    </p>
+                </div>
+
+
+
+                <div class="form-group col-12-xs">
+                    <label for="phoneno">Phoneno<span class="text-error">*</span> </label>
+                    <input type="text" class="form-control" name="phoneno" id="phoneno" placeholder="Enter the phone number" value="" required />
+                    <p class="form-control form-feedback text-error">
+
+                    </p>
+                </div>
+
 
 
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="accountdeletion">
+            <div class="subtitle" id="act_del">
+                <p class="text-gray">Account Deletion </p>
+            </div>
+            <div class="deletiondetails" style="width: 100%;">
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+        </div>
+
+        <div class="deletereason">
+            <div class="subtitle">
+                <p class="text-gray">I am deleting my account because (optional)
+                </p>
+            </div>
+            <textarea style="font-family: poppins; resize:none;" class="text-gray"  id="deletereason" name="deletereason" rows="4" cols="117" >
+</textarea>
+
+
+        </div>
+
+
+
+        <div class="col-4-xs display-f ">
+            <button onclick="location.href='http://localhost/info/help'" class="marketplace__help-btn">Delete My Account </button>
+        </div>
+
+
+    </div>
 
     </div>
 
