9247ed32868a7d1d5a823fd6b3816ed1ebed4130
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-03-29 06:31:42 +0530

    feat: add web manifest files

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -1,3 +1,20 @@
 <link rel="stylesheet" href="<?php echo URLROOT . '/public/updated/css/index.css' ?>" />
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo URLROOT . '/apple-icon-57x57.png' ?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo URLROOT . '/apple-icon-60x60.png' ?>">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo URLROOT . '/apple-icon-72x72.png' ?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo URLROOT . '/apple-icon-76x76.png' ?>">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo URLROOT . '/apple-icon-114x114.png' ?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo URLROOT . '/apple-icon-120x120.png' ?>">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo URLROOT . '/apple-icon-144x144.png' ?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo URLROOT . '/apple-icon-152x152.png' ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo URLROOT . '/apple-icon-180x180.png' ?>">
+<link rel="icon" type="image/png" sizes="192x192" href="<?php echo URLROOT . '/android-icon-192x192.png' ?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo URLROOT . '/favicon-32x32.png' ?>">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo URLROOT . '/favicon-96x96.png' ?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo URLROOT . '/favicon-16x16.png' ?>">
+<link rel="manifest" href="<?php echo URLROOT . '/manifest.webmanifest' ?>">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo URLROOT . '/ms-icon-144x144.png' ?>">
+<meta name="theme-color" content="#ffffff">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
